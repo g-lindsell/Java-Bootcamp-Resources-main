9bd0758a2fa3a37d457eb5c027c0216c052a60d7
Geoff Lindsell <54238446+g-lindsell@users.noreply.github.com>
2026-05-03 15:58:54 +0100

    Initial Commit

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -0,0 +1,20 @@
+public class Championship {
+    public static void main(String[] args) {
+
+        // See Learn the Part for the complete instructions (link in resources folder of
+        // Udemy video).
+
+        int gryffindor = 400; // gryffindor points
+        int ravenclaw = 200; // ravenclaw points
+
+        // int margin = amount of points by which gryffindor scored over ravenclaw;
+        int margin = gryffindor - ravenclaw;
+        if (margin >= 300) {
+            System.out.println("Gryffindor takes the house cup!");
+        } else if (margin >= 0) {
+            System.out.println("In second place, Gryffindor!");
+        } else {
+            System.out.println("In third place, Gryffindor!");
+        }
+    }
+}
