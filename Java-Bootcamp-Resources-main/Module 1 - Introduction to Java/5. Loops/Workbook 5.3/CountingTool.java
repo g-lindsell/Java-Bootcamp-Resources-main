14f3d90ae19c7d297afc0ff8c43914c06b92e0ad
Geoff Lindsell <54238446+g-lindsell@users.noreply.github.com>
2026-05-06 07:41:04 +0100

    Workbook 5.3

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -5,6 +5,11 @@ public class CountingTool {
         Scanner scan = new Scanner(System.in);
 
         // See Learn the Part for the instructions.
+        System.out.print("Hi Timmy! Choose a number to count to: ");
+        int number = scan.nextInt();
+        for (int i = 0; i <= number; i++) {
+            System.out.print(i + " ");
+        }
 
         scan.close();
     }
