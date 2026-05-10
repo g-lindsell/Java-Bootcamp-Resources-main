720a8da28057e17c4611a7294e21e16968fe15f0
Geoff Lindsell <54238446+g-lindsell@users.noreply.github.com>
2026-05-10 08:24:57 +0100

    Workbook 5.11

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
 
-public class Guess  {
+public class Guess {
     public static void main(String[] args) {
 
-       System.out.print("I chose a number between 1 and 5. Try to guess it: ");
-        
-       Scanner scan = new Scanner(System.in);
-       
-       //See Learn the Part for detailed instructions.
-        
+        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
+
+        Scanner scan = new Scanner(System.in);
+
+        // See Learn the Part for detailed instructions.
+        int secret = (int) (5 * Math.random() + 1);
+
+        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
+        int guess = 0;
+        while (guess != secret) {
+            System.out.print("Guess again: ");
+            guess = scan.nextInt();
+            System.out.println(guess);
+        }
+
+        System.out.print("You got it!");
+
         scan.close();
     }
 
--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
@@ -4,31 +4,45 @@ public class SignIn {
     public static void main(String[] args) {
         String username = "Samantha";
         String password = "Java <3";
-        
-        /* Task 1 
-            1. Pick up a username and password from the user.
-       */
+
+        /*
+         * Task 1
+         * 1. Pick up a username and password from the user.
+         */
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
         System.out.print("- Username: ");
-        //pick up username
+        // pick up username
+        String usernameEntry = scan.nextLine();
         System.out.print("- Password: ");
-        //pick up password
+        // pick up password
+        String passwordEntry = scan.nextLine();
 
         // see Learn the Part for the remaining instructions.
-        
-        
-        /* will be useful for task 2
-        
+
+        /*
+         * will be useful for task 2
+         * 
+         * System.out.println("\nIncorrect, please try again!\n");
+         * System.out.print("- Username: ");
+         * usernameEntry = scan.nextLine();
+         * System.out.print("- Password: ");
+         * passwordEntry = scan.nextLine();
+         * 
+         * 
+         */
+
+        while (!usernameEntry.equals(username) || !passwordEntry.equals(password)) {
             System.out.println("\nIncorrect, please try again!\n");
             System.out.print("- Username: ");
             usernameEntry = scan.nextLine();
             System.out.print("- Password: ");
             passwordEntry = scan.nextLine();
+        }
 
-        
-        */
+        System.out.print("Sign in successful. Welcome!");
+
         scan.close();
-        
+
     }
 }
